dd9a789a160f51828f355c0096968e68d9963751
Timothy Makobu <timothy@makobu.name>
2025-10-22 16:36:41 +0300

    green is the primary color now

--- afromart/product/models/product.py
+++ afromart/product/models/product.py
@@ -5,3 +5,12 @@ from django.db import models
 
 class Product(models.Model):
     id = models.UUIDField(default=uuid7, primary_key=True, editable=False)
+    title = models.CharField(verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    in_stock = models.PositiveIntegerField(verbose_name="In Stock")
+    price = models.PositiveBigIntegerField(verbose_name="Price")
+    image = models.URLField(verbose_name="Image")  # to-do: cloudinary field
+    metadata = models.JSONField(verbose_name="Metadata", blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
